6da5fb6bf5400eade2e6c6210dc5bff69cd446b6
Nam Tonthat <ntonthat@squareup.com>
2022-11-27 07:19:05 +1100

    Add notebook for reference

--- ssp.py
+++ ssp.py
@@ -11,6 +11,7 @@ Usage:
 
 Options:
     -u --user           User profile to use [default: default]
+    -st --start-date    Start date of the program [default: today]
     -v --verbose        Verbose logging
     -h --help           Show this screen
 """
@@ -21,8 +22,10 @@ import pandas as pd
 import itertools
 from typing import List, Optional
 from dataclasses import dataclass
+from datetime import datetime, timedelta
 from rich.prompt import Prompt
 import logging
+from prettytable import PrettyTable
 
 # configs
 logging.basicConfig()
@@ -45,6 +48,17 @@ class OneRepMax:
 
     lift: str
     weight: float
+
+    def __post_init__(self):
+        """Clean the names / related compound names"""
+        exercise_name = self.lift.replace("-", " ").title()
+        self.lift = exercise_name
+
+    @property
+    def output_table(self) -> str:
+        """Return a table of the one rep max"""
+        output = [self.lift, self.weight]
+        return output
 
 
 @dataclass
@@ -87,25 +101,65 @@ class Exercise:
         else:
             return self.name
 
-    @property
-    def output_print(self) -> str:
-        """
-        Return the string to output to the user
-        """
+    # @property
+    # def output_print(self) -> str:
+    #     """
+    #     Return the string to output to the user
+    #     """
 
-        outputs = [f"{self.output_name}\n" f"Training Max: {self.predicted_one_rm} kg"]
+    #     outputs = [
+    #         f"\n{self.output_name}\n"
+    #         f"Training Max: {self.predicted_one_rm} kg"]
+    #     if self.top_sets:
+    #         for week, (top, backoff) in enumerate(self.weekly_sets, 1):
+    #             week_date = WEEK_DATES.get(week)
+    #             output = f"Week {week} ({week_date})\n"+\
+    #             f"{top.weight} kg x {top.reps}\n"+\
+    #             f"{backoff.weight} kg x {backoff.reps}"
+    #             outputs.append(output)
+    #     else:
+    #         for week, weight in enumerate(self.weekly_sets, 1):
+    #             week_date = WEEK_DATES.get(week)
+    #             output = f"Week {week} ({week_date})\n"+\
+    #             f"{weight.weight} kg x {weight.reps}"
+    #             outputs.append(output)
+    #     return '\n\n'.join(outputs)
+
+    @property
+    def output_table(self) -> List:
+        "Creates table output for prettytable package"
+        outputs = [f"{self.output_name}"]
         if self.top_sets:
-            for week, (top, backoff) in enumerate(self.weekly_sets, 1):
+            for _, (top, backoff) in enumerate(self.weekly_sets, 1):
                 output = (
-                    f"W{week}: {top.weight} kg x {top.reps}\n"
-                    + f"W{week}: {backoff.weight} kg x {backoff.reps}"
+                    f"{top.weight} kg x {top.reps}\n"
+                    + f"{backoff.weight} kg x {backoff.reps}"
                 )
                 outputs.append(output)
         else:
-            for week, weight in enumerate(self.weekly_sets, 1):
-                output = f"W{week}: {weight.weight} kg x {weight.reps}"
+            for _, weight in enumerate(self.weekly_sets, 1):
+                output = f"{weight.weight} kg x {weight.reps}"
                 outputs.append(output)
-        return "\n".join(outputs)
+
+        return outputs
+
+
+def create_weekly_dates(start_date):
+    """
+    Create the weekly dates for the program
+    """
+    starting_monday = datetime.date(
+        start_date + timedelta(days=7 - start_date.weekday())
+    )
+
+    week_mondays = []
+    for week in range(1, 6):
+        week_monday = starting_monday + timedelta(days=7 * week)
+        week_mondays.append(week_monday)
+
+    # Create the week dates as str
+    week_dates = [i.strftime("%d/%m/%y") for i in week_mondays]
+    return week_dates
 
 
 def calculate_1rm(reps, rpe, weight):
@@ -129,7 +183,7 @@ def round_weights(weight):
     return pl_weight
 
 
-def calculate_training_range(one_rm, reps, rpe_schema) -> List[LiftWeight]:
+def calculate_training_range(one_rm, reps, rpe_schema) -> List[float]:
     """
     Calculate the training day progression for a given 1RM
     """
@@ -143,9 +197,14 @@ def calculate_training_range(one_rm, reps, rpe_schema) -> List[LiftWeight]:
         pct_one_rms.append(pct_one_rm)
         week_weights.append(week_weight)
 
-    # round the weights to the nearest 2.5kg
     training_range = [round_weights(i) for i in week_weights]
 
+    return training_range
+
+
+def create_training_range(one_rm, reps, rpe_schema) -> List[LiftWeight]:
+    """Create a list of LiftWeight objects for the training range"""
+    training_range = calculate_training_range(one_rm, reps, rpe_schema)
     # iterate through training range to make sure there is enough spacing between the numbers
     if has_weekly_difference(training_range):
         LOGGER.info("Weekly difference is too small, recalculating")
@@ -154,13 +213,14 @@ def calculate_training_range(one_rm, reps, rpe_schema) -> List[LiftWeight]:
         updated_rpe_schema = [lowest_rpe] + rpe_schema[1:4] + [highest_rpe]
         training_range = calculate_training_range(one_rm, reps, updated_rpe_schema)
 
-    weights_lifted = list(zip(training_range, itertools.repeat(reps), pct_one_rms))
+    weights_lifted = list(zip(training_range, itertools.repeat(reps)))
     weights_lifted = [LiftWeight(*i) for i in weights_lifted]
 
     return weights_lifted
 
 
 def has_weekly_difference(training_range):
+    "Checks if the weekly progression is less than 2.5 kg"
     weekly_difference = np.diff(training_range).tolist()
     for weekly_diff in weekly_difference:
         if weekly_diff <= 2.5:
@@ -173,7 +233,6 @@ if __name__ == "__main__":
 
     # gather inputs
     MICROPLATES = False
-
     if arguments["--user"]:
         # ask for user profile
         user_profile = Prompt.ask("Enter user profile name", default="default")
@@ -181,6 +240,12 @@ if __name__ == "__main__":
         user_profile = user_profile.replace(" ", "-")
     else:
         user_profile = "default"
+
+    if arguments["--start-date"]:
+        start_date = Prompt.ask(
+            "Enter start date e.g. 2022-11-26 (otherwise defaults to today)",
+            default=datetime.now(),
+        )
 
     LOGGER.info("Reading source data and configs")
     defined_rpes = yaml.load(open("source/rpe.yaml", "r"), Loader=yaml.FullLoader)
@@ -198,10 +263,10 @@ if __name__ == "__main__":
     except FileNotFoundError:
         user_gym = False
 
-    MICROPLATES = user_gym.get("microplates") if user_gym else False
-
     # Global variables
     DF_RPE = pd.read_csv("source/rpe-calculator.csv").set_index("RPE")
+    MICROPLATES = user_gym.get("microplates") if user_gym else False
+    WEEK_DATES = create_weekly_dates(start_date)
 
     max_lifts = []
     full_program = []
@@ -225,13 +290,13 @@ if __name__ == "__main__":
         exercise_volume = exercises.get("program-goals").get(f"{goal}")
 
         if program_type == "strength":
-            top_training_range = calculate_training_range(
+            top_training_range = create_training_range(
                 lift_one_rep_max,
                 reps=exercise_volume.get("top-reps"),
                 rpe_schema=rpes.get("top"),
             )
 
-        backoff_training_range = calculate_training_range(
+        backoff_training_range = create_training_range(
             lift_one_rep_max,
             reps=exercise_volume.get("backoff-reps"),
             rpe_schema=rpes.get("backoff"),
@@ -254,11 +319,11 @@ if __name__ == "__main__":
             min_reps = stats.get("min-reps")
             max_reps = stats.get("max-reps")
 
-            low_volume = calculate_training_range(
+            low_volume = create_training_range(
                 one_rm=accessory_one_rm, reps=min_reps, rpe_schema=accessory_rpe_schema
             )
 
-            high_volume = calculate_training_range(
+            high_volume = create_training_range(
                 one_rm=lift_one_rep_max, reps=max_reps, rpe_schema=accessory_rpe_schema
             )
 
@@ -278,6 +343,19 @@ if __name__ == "__main__":
 
             accessory_program.append(low_volume_exercise)
             accessory_program.append(high_volume_exercise)
-    for exercise in full_program:
-        print(exercise.output_print)
+
+    LOGGER.info("Writing program to file")
+
+    max_strength.output_table
+    main_lifts = PrettyTable()
+
+    # update the week_dates fields
+    WEEK_DATES.insert(0, "Week Starting")
+    main_lifts.add_column("", WEEK_DATES)
+
+    for day_no, exercise in enumerate(full_program, 1):
+        exercise_col = exercise.output_table
+        main_lifts.add_column(f"Day {day_no}", exercise_col)
+
+        # print(exercise.output_print)
     # output training program
